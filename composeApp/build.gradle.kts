2624b82a7e893bde81ae14d852217070dfdd668d
andannn <jqn296763005@gmail.com>
2025-05-05 12:43:56 +0900

    update version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 
     defaultConfig {
         applicationId = "me.andannn.aozora"
-        versionCode = 1
-        versionName = "0.0.1-alpha01"
+        versionCode = 2
+        versionName = "0.0.1-alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
