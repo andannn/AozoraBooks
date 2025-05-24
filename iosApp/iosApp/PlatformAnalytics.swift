38257667318215b2e907b9f0ef37f5fefbb4c074
andannn <jqn296763005@gmail.com>
2025-05-24 16:18:43 +0900

    add ios implement

--- iosApp/iosApp/PlatformAnalytics.swift
+++ iosApp/iosApp/PlatformAnalytics.swift
@@ -1,10 +1,28 @@
 
 import ComposeApp
 import FirebaseAnalytics
+import FirebaseCrashlytics
 
 class IOSAnalytics : PlatformAnalytics {
+    func recordException(throwable: KotlinThrowable) {
+        Crashlytics.crashlytics().record(error: toNSError(throwable: throwable))
+    }
+
     func logEvent(event: String, params: [String : String]) {
         print("invoke log event in swift")
         Analytics.logEvent(event, parameters: params)
+    }
+    
+    private func toNSError(throwable: KotlinThrowable) -> NSError {
+        let message = throwable.message ?? String(describing: self)
+
+        return NSError(
+            domain: "KotlinException",
+            code: 0,
+            userInfo: [
+                NSLocalizedDescriptionKey: message,
+                "KotlinThrowable": self,
+            ]
+        )
     }
 }
