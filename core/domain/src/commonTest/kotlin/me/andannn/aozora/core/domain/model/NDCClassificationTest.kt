65833f6b8652044338a444a1e5c2968829b61557
andannn <jqn296763005@gmail.com>
2025-07-18 09:34:44 +0900

    fix test

--- core/domain/src/commonTest/kotlin/me/andannn/aozora/core/domain/model/NDCClassificationTest.kt
+++ core/domain/src/commonTest/kotlin/me/andannn/aozora/core/domain/model/NDCClassificationTest.kt
@@ -5,6 +5,7 @@
 package me.andannn.aozora.core.domain.model
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFails
 import kotlin.test.assertTrue
 
@@ -61,5 +62,27 @@ class NDCClassificationTest {
         assertTrue(!division.isChildOf(division))
         assertTrue(!section.isChildOf(section))
         assertTrue(!mainClass.isChildOf(mainClass))
+    }
+
+    @Test
+    fun testParseStringAsNDCClassification() {
+        val input1 = "NDC 931"
+        val input2 = "NDC 931 123"
+        val input3 = "NDC K726"
+        val input4 = "NDC 369 453 524"
+
+        val result1 = input1.asNDCClassification()
+        val result2 = input2.asNDCClassification()
+        val result3 = input3.asNDCClassification()
+        val result4 = input4.asNDCClassification()
+
+        assertEquals(1, result1.size)
+        assertEquals("931", result1[0].value)
+        assertEquals(2, result2.size)
+        assertEquals("931", result2[0].value)
+        assertEquals("123", result2[1].value)
+        assertEquals(1, result3.size)
+        assertEquals("726", result3[0].value)
+        assertEquals(3, result4.size)
     }
 }
